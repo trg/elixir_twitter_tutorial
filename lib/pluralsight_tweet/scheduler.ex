9680ff1288396bfc8baff90fc30dbe2aa0d52ca0
Thomas Graft <mr@thomasgraft.com>
2017-09-03 11:41:57 -0400

    init

---
+++ lib/pluralsight_tweet/scheduler.ex
@@ -0,0 +1,8 @@
+defmodule PluralsightTweet.Scheduler do
+    def schedule_file(schedule, file) do
+        Quantum.add_job(
+            schedule,
+            fn -> IO.puts PluralsightTweet.FileReader.get_strings_to_tweet(file) end
+        )
+    end
+end
